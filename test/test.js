9efed81c25484f07917154cf0884da00e90be473
Adrián Mato <yensamg@gmail.com>
2018-05-30 15:08:53 -0700

    Fixed function equal instead of equale

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var assert = require("assert");
 describe("Dependencies", function() {
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
-      assert.equale([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
